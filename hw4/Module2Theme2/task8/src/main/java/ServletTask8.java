feeb1e59bdb69ef9fed339613d2f6b98365ad596
Ivan <Chromer.00@gmail.com>
2021-01-20 18:39:08 +0300

    fixed clas name in hw4

--- hw4/Module2Theme2/task8/src/main/java/ServletTask8.java
+++ hw4/Module2Theme2/task8/src/main/java/ServletTask8.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class servletTask8 extends HttpServlet {
+public class ServletTask8 extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     protected void doGet(HttpServletRequest request, HttpServletResponse
